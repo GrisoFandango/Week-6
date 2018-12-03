7435c4716d754bb5dfb8aaad5af3dae64f20b194
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-03 18:50:17 +0000

    Update and rename 5 - while loop to 5 - while loop.py
    adding comments

---
+++ 5 - while loop.py
@@ -0,0 +1,10 @@
+i = 0 
+#while loop it is execute when the condition is True
+while i < 6:
+  i += 1
+  print(i)
+#while loop it is execute when the condition is True
+i = 0 
+while i < 10:
+  i += 1
+  print("I will always come to class on time")
