93e2932ec8cee7d6fed7401497fefbaac3538d6d
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-03 18:32:34 +0000

    Update and rename 1 - Mark Grades to 1 - Mark Grades.py
    adding comments

--- 1 - Mark Grades.py
+++ 1 - Mark Grades.py
@@ -1,4 +1,8 @@
+#Ask the user to put the mark value
 mark=int(input("Please put your mark:"))
+
+#We verify if the input from the user is enough
+#to pass the course
 if mark >= 40:
     print("Congratulations!!! YOU PASS!")
 else:
