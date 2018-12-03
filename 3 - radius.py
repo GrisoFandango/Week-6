bb23fb9645777cbba9b7d6c9ecf70d92a5e4787c
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-03 18:45:37 +0000

    Update and rename 3 - radius to 3 - radius.py
    adding comments

--- 3 - radius.py
+++ 3 - radius.py
@@ -1,4 +1,7 @@
+#Give to radius value of 5
 radius= 5
+#If the radius is higher than 0 
+#we procedd to calculate the area of the circle.
 if radius >=0:
   area=raius*radius*3.14159
   print("The area of a circle with radius",radius,"is",area)
