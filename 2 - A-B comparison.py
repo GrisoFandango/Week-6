1a7fb0c4750c9caf268cf22bae2420cdc39d61a3
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-03 18:40:29 +0000

    Update and rename 2 - A-B comparison to 2 - A-B comparison.py
    adding comments

--- 2 - A-B comparison.py
+++ 2 - A-B comparison.py
@@ -1,5 +1,8 @@
+#Ask user to give as 2 values
 a=int(input("Please put value for A:"))
 b=int(input("Please put value for B:"))
+#We use if, elif and else to check which
+#number is higher or if they are equal.
 if a > b:
     print("A is greater than B")
 elif a<b:
